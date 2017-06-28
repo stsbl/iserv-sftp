ac0750a83a2d14b88d9306ed1a06b9b091149860
Felix Jacobi <felix.jacobi@stsbl.de>
2017-06-28 12:09:23 +0200

    Removed race condition.

--- iservchk/40sshd/50sftp-modules.sh
+++ iservchk/40sshd/50sftp-modules.sh
@@ -6,10 +6,4 @@ if [ -d "/sftp-chroot/var/lib/iserv/vplan" ]
 then
   echo "ChPerm 0751 root:$GrpVplanAdmin /sftp-chroot/var/lib/iserv/vplan"
 fi
-
-if [ -d "/sftp-chroot/var/lib/iserv/infodisplay" ]
-then
-  echo "ChPerm 0751 root:infobildschirm /sftp-chroot/var/lib/iserv/infodisplay"
-fi
-
 echo
